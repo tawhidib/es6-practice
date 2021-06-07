6157ff172ba375b1a6ed4ef7add71732489185ba
tawhidib <tawhidib@gmail.com>
2021-06-07 11:56:13 +0600

    string concatenation and multiple line using TEMPLATE

---
+++ template.js
@@ -0,0 +1,25 @@
+// Template ব্যবহার করে string জোড়া লাগান যায়...
+
+const firstName = "Md Towhidul";
+const lastName = "Islam";
+const birthYear = 1995;
+const nickName = "Babu";
+
+const message = `My name is ${firstName} ${lastName}. My age is ${
+	2021 - birthYear
+}. My nick name is ${nickName}.`;
+
+console.log(message);
+
+// Template ব্যবহার করে multiline print করা যায়...
+
+const mulLine = `
+**************************************
+
+ami shakko dicchi j ALLAH chara kono mabud nei...
+ar ami eitao shakkho dicchi j Hazrat Muhammad SM allah perito rasul...
+
+**************************************
+`;
+
+console.log(mulLine);
